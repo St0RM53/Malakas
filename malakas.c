bad357b9495638f5eb2dfd414c2425a8d39b88ae
St0RM53 <51150391+St0RM53@users.noreply.github.com>
2019-05-31 11:07:23 +0300

    Add files via upload

---
+++ malakas.c
@@ -0,0 +1,19 @@
+#include "malakas.h"
+
+/*
+__attribute__ ((weak))
+void matrix_init_user(void) {
+}
+
+__attribute__ ((weak))
+void matrix_scan_user(void) {
+}
+
+void matrix_init_kb(void) {
+  matrix_init_user();
+};
+
+void matrix_scan_kb(void) {
+  matrix_scan_user();
+};
+*/
---
+++ malakas.h
@@ -0,0 +1,16 @@
+/*#ifndef KEYBOARDS_MALAKAS_H
+#define KEYBOARDS_MALAKAS_H
+*/
+#pragma once
+#include "quantum.h"
+
+#define LAYOUT( \
+	K00, K01, K02, \
+	K10, K11, K12  \
+) { \
+	{ K00,   K01,   K02 }, \
+	{ K10,   K11,   K12 }  \
+}
+/*
+#endif
+*/
